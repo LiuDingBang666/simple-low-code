931535158aaaf4aaa2377b298a86496b40099c20
ldb <2993442750@qq.com>
2025-09-01 16:17:56 +0800

    feat(draw): 初步设计拖拽低代码设计器布局
    - 添加头部、组件栏、内容区、设置栏等区域
    - 设置各区域样式和布局比例
    - 添加全局样式，设置 app 宽高

--- draw-low-code/src/pages/draw/DrawLowCode.vue
+++ draw-low-code/src/pages/draw/DrawLowCode.vue
@@ -1,17 +1,38 @@
 <!--
-@description: TODO
+@description: TODO 脱拽低代码设计器
 @author: liudingbang
 @date: 2025/9/1 14:38
 -->
 
 <template>
-  <el-row>
-    <el-col :span="6">1</el-col>
-    <el-col :span="12">2</el-col>
-    <el-col :span="6">3</el-col>
+  <el-row class="header">
+    <el-col class="header-column" :span="24">头部</el-col>
+  </el-row>
+  <el-row class="content">
+    <el-col class="components" :span="6"></el-col>
+    <el-col class="content" :span="12"></el-col>
+    <el-col class="setting" :span="6"></el-col>
   </el-row>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.header {
+  height: 10vh;
+
+  .header-column {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+.content {
+  height: 90vh;
+}
+
+.el-col {
+  border: 0.5px solid rgba(128, 128, 128, 0.8);
+}
+</style>
