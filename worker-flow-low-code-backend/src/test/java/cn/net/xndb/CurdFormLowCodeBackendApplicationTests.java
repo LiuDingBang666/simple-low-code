ab222ad06841f2477e9876f58a74289f9c48789b
ldb <2993442750@qq.com>
2025-09-10 16:32:11 +0800

    feat(worker-flow-low-code): 新增低代码平台项目
    - 添加前端项目结构和基本配置
    - 创建后端项目结构和基本配置- 实现简单的 HTTP 请求工具- 添加路由配置和主应用文件
    - 配置 ESLint 和 Prettier
    - 创建 Git仓库并添加初始提交

---
+++ worker-flow-low-code-backend/src/test/java/cn/net/xndb/CurdFormLowCodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.xndb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdFormLowCodeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
